d546f0f3c849967a05a4859860cdec7e58f9a1aa
mohammadtonor <mtonor1368@gmail.com>
2024-07-03 20:59:09 +0330

    landing page comlette

--- src/app/(main)/agency/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(main)/agency/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,9 @@
-import { SignUp } from '@clerk/nextjs'
+import { SignIn } from '@clerk/nextjs'
 import React from 'react'
 
 const page = () => {
   return (
-    <SignUp />
+    <SignIn />
   )
 }
 
